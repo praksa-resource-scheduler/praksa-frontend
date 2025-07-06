2f98c82c17c64cc1cf16bb124c40ada7292a9183
manje22 <uvodicmariela@gmail.com>
2025-07-06 19:56:31 +0200

    added text area for rezervation description

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -6,7 +6,7 @@ export default function Reservation() {
       </h2>
       <form className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
         <label className="block mb-4">
-          <span className="text-gray-700 font-semibold">Profesor</span>
+          <span className="text-gray-700 font-semibold">Profesor ili ID</span>
           <input
             type="text"
             name="professor"
@@ -64,9 +64,22 @@ export default function Reservation() {
           </label>
         </div>
 
+        <div>
+          <label className="flex-1">
+            <span className="text-gray-700 font-semibold">
+              Svrha rezervacije
+            </span>
+            <textarea
+              required
+              placeholder="Unesite svrhu rezervacije"
+              className="mt-1 block w-full rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:ring focus:ring-blue-200"
+            ></textarea>
+          </label>
+        </div>
+
         <button
           type="submit"
-          className="w-full bg-blue-600 text-white py-2 rounded-lg font-semibold hover:bg-blue-700 transition"
+          className="w-full mt-10 bg-blue-600 text-white py-2 rounded-lg font-semibold hover:bg-blue-700 transition"
         >
           Rezerviraj
         </button>
