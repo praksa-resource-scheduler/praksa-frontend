0f45419cd22c973679dabce0ed64159dc89a7210
Rei0101 <62380465+Rei0101@users.noreply.github.com>
2025-10-28 22:51:35 +0100

    Enable registration functionality using firebase

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,26 +1,72 @@
+import { useState } from "react";
+import { Navigate, Link } from "react-router-dom";
+import { useAuth } from "../contexts/authContext";
+import { doCreateUserWithEmailAndPassword } from "../firebase/auth";
 import AuthInput from "../components/AuthInput";
 import AuthButton from "../components/AuthButton";
 
 export default function Register() {
+  const { userLoggedIn } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (isRegistering) return;
+
+    setIsRegistering(true);
+
+    try {
+      await doCreateUserWithEmailAndPassword(email, password);
+    } catch {
+      setIsRegistering(false);
+    }
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center  px-4">
-      <div className="max-w-md w-full bg-white p-10 rounded-3xl shadow-2xl border border-gray-200">
-        <div className="text-center mb-8">
-          <h2 className="text-4xl font-extrabold text-gray-800">
-            Resource Scheduler
-          </h2>
-          <p className="text-sm text-gray-500 mt-1">Register</p>
+    <>
+      {userLoggedIn && <Navigate to={"/"} replace />}
+
+      <div className="min-h-screen flex items-center justify-center  px-4">
+        <div className="max-w-md w-full bg-white p-10 rounded-3xl shadow-2xl border border-gray-200">
+          <div className="text-center mb-8">
+            <h2 className="text-4xl font-extrabold text-gray-800">
+              Resource Scheduler
+            </h2>
+            <p className="text-sm text-gray-500 mt-1">Register</p>
+          </div>
+
+          <form onSubmit={onSubmit} className="space-y-6">
+            <AuthInput
+              label="E-mail"
+              type="email"
+              placeholder="name@gmail.com"
+              state={email}
+              setState={setEmail}
+            />
+            <AuthInput
+              label="Password"
+              type="password"
+              placeholder="••••••••"
+              state={password}
+              setState={setPassword}
+            />
+
+            <div className="text-center mt-2"></div>
+
+            <AuthButton text={isRegistering ? "Registering..." : "Register"} />
+            <p className="text-center text-sm">
+              Already have an account?
+              <Link to={"/login"} className="hover:underline font-bold">
+                {" "}
+                Login now!
+              </Link>
+            </p>
+          </form>
         </div>
-
-        <form className="space-y-6">
-          <AuthInput label="E-mail" type="email" placeholder="name@gmail.com" />
-          <AuthInput label="Password" type="password" placeholder="••••••••" />
-
-          <div className="text-center mt-2"></div>
-
-          <AuthButton text="Register" />
-        </form>
       </div>
-    </div>
+    </>
   );
 }
